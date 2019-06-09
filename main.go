11209bc7191528a69842b2432070027b2189e951
王华 <wanghua@weidian.com>
2019-06-09 22:20:16 +0800

    test static check

--- main.go
+++ main.go
@@ -14,5 +14,15 @@ func main() {
 
 	fmt.Println("jjjjjk")
 	fmt.Println("hehheh")
-	fmt.Println("success")
+	if check(11) {
+		fmt.Println("success")
+	}
+}
+
+func check(a int) bool {
+	if a > 10 {
+		return true
+	}
+
+	return false
 }
