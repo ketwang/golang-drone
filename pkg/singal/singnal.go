b6b29a083e2dc23651515cea82244ffe49427e04
王华 <wanghua@weidian.com>
2019-06-14 13:54:53 +0800

    add pkg/signal

---
+++ pkg/singal/singnal.go
@@ -0,0 +1,59 @@
+package singal
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var (
+	// hup used for reload configuration
+	reloadingSignalList = []os.Signal{syscall.SIGHUP}
+	terminationSignalList =  []os.Signal{syscall.SIGINT, syscall.SIGTERM}
+)
+
+func init()  {
+	//default handling: ignore sig hup signal
+	signal.Ignore(reloadingSignalList...)
+}
+
+func WitchSingalsContext(ctx context.Context) context.Context {
+	newCtx, cancel := context.WithCancel(ctx)
+
+	// define chan to receive signals
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, terminationSignalList...)
+
+	// goroute waiting for upper ctx done event
+	go func() {
+		defer cancel()
+
+		select {
+		case <- ctx.Done():
+		case <- sig:
+		}
+	}()
+
+	return newCtx
+}
+
+func WithReloadingContext() context.Context {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// reset sig hub handling
+	sig := make(chan os.Signal, 1)
+	signal.Reset(reloadingSignalList...)
+	signal.Notify(sig, reloadingSignalList...)
+
+	go func() {
+		defer cancel()
+
+		select {
+		case <- ctx.Done():
+		case <- sig:
+		}
+	}()
+
+	return ctx
+}
