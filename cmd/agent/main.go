5441c6abc28bdb2833c848b1006a442c97e28d22
王华 <wanghua@weidian.com>
2019-06-14 14:03:38 +0800

    update cmd/agent/main

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,50 +1,19 @@
 package main
 
 import (
-	"fmt"
-	ui "github.com/gizak/termui/v3"
-	"math/rand"
-	"time"
-	"util/pkg/convert"
-	"util/pkg/termui"
+	"github.com/spf13/cobra"
+	"os"
+	"util/version"
 )
 
 func main() {
-	if err := ui.Init(); err != nil {
-		fmt.Println(err)
-		return
+	rootCmd := &cobra.Command{
+		Use: "util-agent",
 	}
 
-	format := func(key string, value []float64) string {
-		if len(value) == 0 {
-			return key
-		}
+	rootCmd.AddCommand(version.NewCommand())
 
-		v := value[len(value)-1]
-		return fmt.Sprintf("%s: %s/s", key, convert.Size(v).String())
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
 	}
-
-	lw := termui.NewLineWidget(100, format)
-	lw.Title = "hahahaha"
-
-	entries := make([]interface{}, 0)
-	entries = append(entries, ui.NewRow(2.0/3, ui.NewCol(1.0, lw)))
-
-	termWidth, termHeight := ui.TerminalDimensions()
-	grid := ui.NewGrid()
-	grid.SetRect(0, 0, termWidth, termHeight)
-	grid.Set(entries...)
-
-	timer := time.NewTicker(5 * time.Second)
-
-	for {
-		lw.Update(map[string]float64{"rx": float64(rand.Intn(100))}, false)
-		ui.Render(grid)
-		select {
-		case <-timer.C:
-		case <-ui.PollEvents():
-			return
-		}
-	}
-
 }
